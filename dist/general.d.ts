075c14030dd857fc19bf694f31806b39498905d3
Ben Schmidt <bmschmidt@gmail.com>
2023-06-23 11:39:08 -0400

    no dist in git

--- dist/general.d.ts
+++
@@ -1,7 +0,0 @@
-import type { AtlasUser } from "./user.js";
-export declare const isNode: string | false;
-export declare abstract class BaseAtlasClass {
-    user: AtlasUser;
-    constructor(user?: AtlasUser);
-    apiCall(endpoint: string, method: "GET" | "POST", payload?: Atlas.Payload, headers?: null | Record<string, string>): Promise<Response>;
-}
--- dist/main.d.ts
+++
@@ -1,4 +0,0 @@
-export { AtlasProject, create_project } from "./project.js";
-export { AtlasUser, AtlasOrganization } from "./user.js";
-export { AtlasProjection } from "./projection.js";
-export { AtlasIndex } from "./index.js";
