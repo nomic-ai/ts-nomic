1c77f6ac0cf908b3d81e63c52f53480564978663
Robert Lesser <bclesser@gmail.com>
2023-09-20 17:36:33 -0400

    Add APIError to main export

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 export { AtlasProject } from './project.js';
-export { AtlasUser } from './user.js';
+export { AtlasUser, APIError } from './user.js';
 export { AtlasOrganization } from './organization.js';
 export { AtlasProjection } from './projection.js';
 export { AtlasIndex } from './index.js';
