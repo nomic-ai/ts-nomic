af0acb7e6900deb9145b0a123febe71cbe7e41b2
Ben Schmidt <bmschmidt@gmail.com>
2024-02-28 12:25:26 -0500

    v 0.9.2

--- tests/embedding.test.js
+++ tests/embedding.test.js
@@ -20,19 +20,39 @@ test('Embed a point from an env token', async () => {
 
   // Taking the dot product and assert sentences 0 and 1 are closer than sentences 0 and 2
 
+  function dot(a, b) {
+    let sum = 0;
+    for (let i = 0; i < a.length; i++) {
+      sum += a[i] * b[i];
+    }
+    return sum;
+  }
   const similarityMatrix = [];
   for (let i = 0; i < values.length; i++) {
     const ds = [];
     for (let j = 0; j < values.length; j++) {
-      let d = 0;
-      for (let n = 0; n < values[0].length; n++) {
-        d += values[i][n] * values[j][n];
-      }
-      ds.push(d);
+      ds.push(dot(values[i], values[j]));
     }
     similarityMatrix.push(ds);
   }
   assert.is(similarityMatrix[0][1] > similarityMatrix[0][2], true);
+
+  const olderVersion = new Embedder(process.env.ATLAS_API_KEY, {
+    model: 'nomic-embed-text-v1',
+    taskType: 'search_document',
+  });
+  const differentTerms = new Embedder(process.env.ATLAS_API_KEY, {
+    model: 'nomic-embed-text-v1',
+    taskType: 'search_query',
+  });
+
+  const dogs1 = await olderVersion.embed(strings[0]);
+  const dogs2 = await differentTerms.embed(strings[0]);
+
+  assert.is(dogs1.length, 768);
+  assert.is(dogs2.length, 768);
+  assert.is(dot(dogs1, dogs2) < 0.9, true);
+  assert.is(dot(values[0], dogs1) < 0.9, true);
 });
 
 test.run();
